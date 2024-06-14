75a685c7bb59027735e9a934b693e49766b32e9d
FEROZ130405 <160384048+FEROZ130405@users.noreply.github.com>
2024-06-14 15:30:43 +0530

    Add files via upload

--- custom-hooks/src/App.js
+++ custom-hooks/src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import CounterComponent from './CounterComponent';
 
+const App = () => {
+  return (
+    <div>
+      <CounterComponent />
+    </div>
+  );
+};
+
+export default App;
