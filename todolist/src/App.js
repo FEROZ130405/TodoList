b346509b0e31f57a00d234b63a839a5e1cba0460
FEROZ130405 <160384048+FEROZ130405@users.noreply.github.com>
2024-06-14 15:32:59 +0530

    Add files via upload

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,1 +1,27 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+import './App.css';
+function App() {
+  const [todos, setTodos] = useState([]);
 
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text, id: Date.now() }];
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (id) => {
+    const newTodos = todos.filter(todo => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="App">
+      <h1>To-Do List</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} removeTodo={removeTodo} />
+    </div>
+  );
+}
+
+export default App;
