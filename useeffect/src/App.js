99602638a6e82d10237915a4b36e1a8f5b67ede8
FEROZ130405 <160384048+FEROZ130405@users.noreply.github.com>
2024-06-14 15:19:54 +0530

    Add files via upload

--- useeffect/src/App.js
+++ useeffect/src/App.js
@@ -1,1 +1,21 @@
+import React, { useState, useEffect } from 'react';
 
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
+    
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+    </div>
+  );
+};
+
+export default App;
